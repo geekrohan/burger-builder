8a8c105a0bc26eef58cfe15be28bf87599a64f04
geekrohan <geekboyrohan@gmail.com>
2020-12-30 02:07:37 +0530

    added logo

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
-import classes from './Toolbar.module.css'
+import classes from './Toolbar.module.css';
+import Logo from '../../UI/Logo/Logo';
 
 const toolbar = (props) => {
 
     return (
         <header className={classes.Toolbar}>
             <div>MENU</div>
-            <div>LOGO</div>
+            <Logo></Logo>
             <nav>
                 ...
         </nav>
---
+++ src/components/UI/Logo/Logo.js
@@ -0,0 +1,12 @@
+import burgerLogo from '../../../assets/images/burger-logo.png';
+import classes from './Logo.module.css';
+
+const logo = (props) => {
+    return (
+        <div className={classes.Logo}>
+            <img src={burgerLogo} alt="" />
+        </div>
+    )
+}
+
+export default logo;
