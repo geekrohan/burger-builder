eef746d01d98aba46ac26b91c84ed07c70d18580
geekrohan <geekboyrohan@gmail.com>
2020-12-30 01:54:40 +0530

    added toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import Aux from '../../hoc/Auxiliary';
 import classes from './layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => {
     return (
         <Aux>
-            <div>Toolbar, Side-drawer, Backdrop</div>
+            <Toolbar></Toolbar>
             <main className={classes.Content}>{props.children}</main>
         </Aux>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -109,6 +109,7 @@ class BurgerBuilder extends Component {
                     <OrderSummary ingredients={this.state.ingredients}
                         purchaseCancelled={this.purchaseCancelHandler}
                         purchaseContinued={this.purchaseContinueHandler}
+                        price={this.state.totalPrice}
                     ></OrderSummary>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}></Burger>
